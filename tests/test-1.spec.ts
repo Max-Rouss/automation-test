ff2e654103be0679c1fd6e2e34acac0cfbcf4312
Maxime Rousseau <mrousseau@recursyve.io>
2024-07-08 14:50:41 -0400

    Update

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -14,8 +14,8 @@ test('test', async ({ page }) => {
   await page.locator('#mat-select-value-7').click();
   await page.getByRole('option', { name: 'Italian' }).click();
   await page.getByPlaceholder('Rechercher l\'adresse').fill('1235');
-  await page.getByRole('option', { name: '1235 Wilson Avenue North York' }).click();
- /*
+  /*
+  await page.getByRole('option', { name: '1235 Wilson Avenue North York' }).click(); 
   await page.getByPlaceholder('Prénom').fill('Maxime');
   await page.getByPlaceholder('Nom', { exact: true }).fill('ER');
   await page.getByPlaceholder('exemple@courriel.com').fill('mrousseau+11@recursyve.io');
