adbf38dd82365e26ba7d0f140f21bc2dd5a63612
Maxime Rousseau <mrousseau@recursyve.io>
2024-07-10 13:15:28 -0400

    Update

--- tests/FormateurPlus_VitrineUI.spec.ts
+++ tests/FormateurPlus_VitrineUI.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 
 test('test', async ({ page }) => {
   //initial config, responsive layout 1
@@ -45,30 +45,48 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'Test your digital skills' }).press('Tab');
   await page.getByRole('link', { name: 'Test your digital skills' }).click({trial: true});
 
+
   //responsive layout 2 (1536px)
   await page.setViewportSize({ width: 1535, height: 1080 });
   await page.pause();
-  await page.getByRole('button').nth(0).press('Tab');
   await page.getByRole('button').nth(0).click();
 
   //Find and check clickable locators in hamburger menu
+  await page.getByRole('textbox', { name: 'Search' }).nth(1).press('Tab');
+  await page.getByRole('textbox', { name: 'Search' }).nth(1).click({trial: true});
 
+  await page.getByRole('button').nth(8).press('Tab');
+  await page.getByRole('button').nth(8).click();
+  await expect(page.getByText('Content')).toBeVisible(); //flake possible
+
+  await page.getByRole('button').nth(9).press('Tab');
+  await page.getByRole('button').nth(9).click();
+ // await expect(page.getByText('Content')).toBeVisible(); //flake possible
+
+  await page.getByRole('button', { name: 'My space' }).press('Tab');
+  await page.getByRole('button', { name: 'My space' }).click({trial: true});
+
+  await page.getByRole('button', { name: 'Are you a company?' }).press('Tab');
+  await page.getByRole('button', { name: 'Are you a company?' }).click({trial: true});
+
+  await page.getByRole('heading', { name: 'logo' }).getByRole('button').click();
 
   //responsive layout 3 (768px)
   await page.setViewportSize({ width: 767, height: 1080 });
   await page.pause();
-  await page.getByRole('button').nth(0).press('Tab');
   await page.getByRole('button').nth(0).click();
 
   //Find and check clickable locators in hamburger menu
-  await page.getByRole('textbox', { name: 'Search' }).press('Tab');
-  await page.getByRole('textbox', { name: 'Search' }).click({trial: true});
+  await page.getByRole('textbox', { name: 'Search' }).nth(0).press('Tab');
+  await page.getByRole('textbox', { name: 'Search' }).nth(0).click({trial: true});
 
-  await page.getByRole('button', { name: 'Text trigger' }).nth(0).press('Tab');
-  await page.getByRole('button', { name: 'Text trigger' }).nth(0).click({trial: true});
+  await page.getByRole('button').nth(5).press('Tab');
+  await page.getByRole('button').nth(5).click();
+  await expect(page.getByText('Content')).toBeVisible(); //flake possible
 
-  await page.getByRole('button', { name: 'Text trigger' }).nth(1).press('Tab');
-  await page.getByRole('button', { name: 'Text trigger' }).nth(1).click({trial: true});
+  await page.getByRole('button').nth(6).press('Tab');
+  await page.getByRole('button').nth(6).click();
+ // await expect(page.getByText('Content')).toBeVisible(); //falke possible
 
   await page.getByRole('button', { name: 'My space' }).press('Tab');
   await page.getByRole('button', { name: 'My space' }).click({trial: true});
