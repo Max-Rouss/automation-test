ad536c48b494eda276c17d669b81d335e5ee537e
Maxime Rousseau <mrousseau@recursyve.io>
2024-07-12 09:24:16 -0400

    Update

--- tests/FormateurPlus_VitrineUI-TabletP.spec.ts
+++ tests/FormateurPlus_VitrineUI-TabletP.spec.ts
@@ -2,10 +2,19 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('https://formateur-plus.vitrine.recursyve.dev/');
-  await page.setViewportSize({ width: 767, height: 1080 });
+  await page.setViewportSize({ width: 1024, height: 1080 });
   await page.pause();
   
   await page.getByRole('button').nth(0).click();
+
+  /*
+  locator.click() ensures 
+  - that locator resolves to exactly one element
+  - element is Visible
+  - element is Stable, as in not animating or completed animation
+  - element Receives Events, as in not obscured by other elements
+  - element is Enabled
+  */
 
   //Find and check clickable locators in hamburger menu
   await page.getByRole('textbox', { name: 'Search' }).nth(0).press('Tab');
